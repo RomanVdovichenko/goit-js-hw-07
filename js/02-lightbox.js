bd05c774895dc9825e932c157585ed1c2723e019
Roman Vdovichenko <vrn6870@gmail.com>
2023-02-17 07:36:43 +0200

    new

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -8,13 +8,13 @@ const galleryEl = document.querySelector(".gallery");
 
 const markup = galleryItems
   .map(({ original, preview, description }) => {
-    return `<a class="gallery__item" href="${original}">
+    return `<li><a class="gallery__item" href="${original}">
         <img
           class="gallery__image"
           src="${preview}"
           alt="${description}"
         />
-      </a>`;
+      </a></li>`;
   })
   .join("");
 
